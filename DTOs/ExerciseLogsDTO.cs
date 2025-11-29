a256495c13554acef7b6e17aba1f9721fa010287
Ivan Zamishchak <121110260+erfub1120@users.noreply.github.com>
2025-11-30 00:24:42 +0100

    Updated DTOs

--- DTOs/ExerciseLogsDTO.cs
+++ DTOs/ExerciseLogsDTO.cs
@@ -5,7 +5,30 @@ namespace BeFit.DTOs
 {
     public class ExerciseLogsDTO
     {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Exercise Type Id")]
         [Required]
-        
+        public int ExerciseTypeId { get; set; }
+
+        [Display(Name = "Training Session Id")]
+        [Required]
+        public int TrainingSessionId { get; set; }
+
+        [Display(Name = "Weight")]
+        [Required]
+        [Range(0, 1000)]
+        public decimal Weight { get; set; }
+
+        [Display(Name = "Sets")]
+        [Required]
+        [Range(1, 100)]
+        public int Sets { get; set; }
+
+        [Display(Name = "Repetitions")]
+        [Required]
+        [Range(1, 500)]
+        public int Reps { get; set; }
     }
 }
---
+++ DTOs/TrainingSessionDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeFit.DTOs
+{
+    public class TrainingSessionDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Start time")]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Display(Name = "End time")]
+        public DateTime EndTime { get; set; }
+    }
+}
